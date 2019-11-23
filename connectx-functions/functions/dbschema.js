7c573fd04bac8d3bf2e16e9778fd9d154e260301
shubham <chd.nonu26@gmail.com>
2019-11-23 13:13:40 +0530

    databse API with firebase

---
+++ connectx-functions/functions/dbschema.js
@@ -0,0 +1,13 @@
+let db={
+    screams:[
+
+        {
+            userHandle:'user',
+            body:'this is the scream body',
+            createdAt:"2019-11-22T08:56:32.188Z",
+            likecount:5,
+            commentcount:2
+
+        }
+    ]
+}
